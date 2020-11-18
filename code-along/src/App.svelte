d6acefbd3ae6957885f8d649f0260f479ac3418d
edward-hong <edward.hong527@gmail.com>
2020-11-18 13:10:56 +1300

    S02L05 Binding to Element Properties

--- code-along/src/App.svelte
+++ code-along/src/App.svelte
@@ -16,6 +16,11 @@
   function changeName() {
     name = 'Ed'
   }
+
+  function nameInput(event) {
+    const enteredValue = event.target.value
+    name = enteredValue
+  }
 </script>
 
 <style>
@@ -27,5 +32,6 @@
 <main>
   <h1>Hello {uppercaseName}, my age is {age}!</h1>
   <button on:click={incrementAge}>Change Age</button>
-  <button on:click={changeName}>Change Name</button>
+  <!-- <button on:click={changeName}>Change Name</button> -->
+  <input type="text" value={name} on:input={nameInput} />
 </main>
