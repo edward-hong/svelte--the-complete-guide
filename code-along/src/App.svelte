61f42093d2e3d89527fa3d39f0a7b737bee975f8
edward-hong <edward.hong527@gmail.com>
2020-11-18 13:21:10 +1300

    S02L07 Using Multiple Components

--- code-along/src/App.svelte
+++ code-along/src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import ContactCard from './ContactCard.svelte'
+
   let name = 'Edward'
   let age = 30
 
@@ -29,10 +31,9 @@
   }
 </style>
 
-<main>
-  <h1>Hello {uppercaseName}, my age is {age}!</h1>
-  <button on:click={incrementAge}>Change Age</button>
-  <!-- <button on:click={changeName}>Change Name</button> -->
-  <!-- <input type="text" value={name} on:input={nameInput} /> -->
-  <input type="text" bind:value={name} />
-</main>
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
+<ContactCard />
