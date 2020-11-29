7f81bbce8819760c9ab81bc1603358d4a0e4883b
edward-hong <edward.hong527@gmail.com>
2020-11-30 02:10:19 +1300

    S03L02 Using "if" Statements in HTML

--- code-along/src/App.svelte
+++ code-along/src/App.svelte
@@ -5,41 +5,45 @@
   let title = ''
   let image = ''
   let description = ''
-  let age = 30
+  let done = false
 
-  $: uppercaseName = name.toUpperCase()
-  $: console.log(name)
-  $: if (name === 'Ed') {
-    console.log('It runs!')
-    age = 31
-  }
-
-  function incrementAge() {
-    age += 1
-  }
-
-  function changeName() {
-    name = 'Ed'
-  }
-
-  function nameInput(event) {
-    const enteredValue = event.target.value
-    name = enteredValue
+  function addContact() {
+    done = true
   }
 </script>
 
 <style>
-  h1 {
-    color: purple;
+  #form {
+    width: 30rem;
+    max-width: 100%;
   }
 </style>
 
-<h1>Hello {uppercaseName}, my age is {age}!</h1>
-<button on:click={incrementAge}>Change Age</button>
-<!-- <button on:click={changeName}>Change Name</button> -->
-<!-- <input type="text" value={name} on:input={nameInput} /> -->
-<input type="text" bind:value={name} />
-<input type="text" bind:value={title} />
-<input type="text" bind:value={image} />
-<input type="text" bind:value={description} />
-<ContactCard userName={name} jobTitle={title} {description} userImage={image} />
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<button on:click={addContact}>Add Contact Card</button>
+
+{#if done}
+  <ContactCard
+    userName={name}
+    jobTitle={title}
+    {description}
+    userImage={image} />
+{/if}
