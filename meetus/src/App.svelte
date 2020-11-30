7c6486db1a1ddfb8fff28610669c8ce097f7698b
edward-hong <edward.hong527@gmail.com>
2020-11-30 19:07:04 +1300

    S05L10 Creating a "TextInput" Component

--- meetus/src/App.svelte
+++ meetus/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from './UI/Header.svelte'
   import MeetupGrid from './Meetups/MeetupGrid.svelte'
+  import TextInput from './UI/TextInput.svelte'
 
   let title = ''
   let subtitle = ''
@@ -52,36 +53,54 @@
   main {
     margin-top: 5rem;
   }
+
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
 </style>
 
 <Header />
 
 <main>
   <form on:submit|preventDefault={addMeetup}>
-    <div class="form-control">
-      <label for="title">Title</label>
-      <input type="text" id="title" bind:value={title} />
-    </div>
-    <div class="form-control">
-      <label for="subtitle">Subtitle</label>
-      <input type="text" id="subtitle" bind:value={subtitle} />
-    </div>
-    <div class="form-control">
-      <label for="address">Address</label>
-      <input type="text" id="address" bind:value={address} />
-    </div>
-    <div class="form-control">
-      <label for="imageUrl">Image URL</label>
-      <input type="text" id="imageUrl" bind:value={imageUrl} />
-    </div>
-    <div class="form-control">
-      <label for="email">E-Mail</label>
-      <input type="email" id="email" bind:value={email} />
-    </div>
-    <div class="form-control">
-      <label for="description">Description</label>
-      <textarea rows="3" id="description" bind:value={description} />
-    </div>
+    <TextInput
+      id="title"
+      label="Title"
+      value={title}
+      type="text"
+      on:input={(e) => (title = e.target.value)} />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      value={subtitle}
+      type="text"
+      on:input={(e) => (subtitle = e.target.value)} />
+    <TextInput
+      id="address"
+      label="Address"
+      value={address}
+      type="text"
+      on:input={(e) => (address = e.target.value)} />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      value={imageUrl}
+      type="text"
+      on:input={(e) => (imageUrl = e.target.value)} />
+    <TextInput
+      id="email"
+      label="E-Mail"
+      value={email}
+      type="email"
+      on:input={(e) => (email = e.target.value)} />
+    <TextInput
+      id="description"
+      label="Description"
+      value={description}
+      controlType="textarea"
+      on:input={(e) => (description = e.target.value)} />
     <button type="submit">Save</button>
   </form>
   <MeetupGrid {meetups} />
