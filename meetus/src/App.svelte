e22631b80ea45fb499d3a4e13c167a16e1da1355
edward-hong <edward.hong527@gmail.com>
2020-12-01 17:55:55 +1300

    S07L03 Communication via Custom Events

--- meetus/src/App.svelte
+++ meetus/src/App.svelte
@@ -22,6 +22,7 @@
         'https://upload.wikimedia.org/wikipedia/commons/7/75/Sutton_High_Street%2C_Sutton%2C_London%2C_Caffe_Nero.jpg',
       address: '27th Nerd Road, 32523 New York',
       contactEmail: 'code@test.com',
+      isFavourite: false,
     },
     {
       id: 'm2',
@@ -32,6 +33,7 @@
         'https://upload.wikimedia.org/wikipedia/commons/d/d1/Baku_Aquatic_Palace%2C_Olympic_Pool.jpg',
       address: '7th Swim Road, 53212 Brooklyn',
       contactEmail: 'swim@test.com',
+      isFavourite: false,
     },
   ]
 
@@ -47,6 +49,16 @@
     }
 
     meetups = [newMeetup, ...meetups]
+  }
+
+  function toggleFavourite(event) {
+    const id = event.detail
+    const updatedMeetup = { ...meetups.find((m) => m.id === id) }
+    updatedMeetup.isFavourite = !updatedMeetup.isFavourite
+    const meetupIndex = meetups.findIndex((m) => m.id === id)
+    const updatedMeetups = [...meetups]
+    updatedMeetups[meetupIndex] = updatedMeetup
+    meetups = updatedMeetups
   }
 </script>
 
@@ -100,5 +112,5 @@
       on:input={(e) => (description = e.target.value)} />
     <Button type="submit" caption="Save" />
   </form>
-  <MeetupGrid {meetups} />
+  <MeetupGrid {meetups} on:togglefavourite={toggleFavourite} />
 </main>
