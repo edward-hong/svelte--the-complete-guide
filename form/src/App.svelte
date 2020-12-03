fe1b4d97eaad03a8e59780b40c71d0170035e559
edward-hong <edward.hong527@gmail.com>
2020-12-03 21:12:33 +1300

    S08L05 Binding Checkboxes & Radio Buttons

--- form/src/App.svelte
+++ form/src/App.svelte
@@ -5,10 +5,14 @@
   let val = 'Edward'
   let selectedOption = 1
   let price = 0
+  let agreed
+  let favColor = ['green']
 
   $: console.log(val)
   $: console.log(selectedOption)
   $: console.log(price)
+  $: console.log(agreed)
+  $: console.log(favColor)
 
   function setValue(event) {
     val = event.target.value
@@ -26,3 +30,22 @@
 <Toggle bind:chosenOption={selectedOption} />
 
 <input type="number" bind:value={price} />
+
+<label>
+  <input type="checkbox" bind:checked={agreed} />
+  Agree to terms?
+</label>
+
+<h1>Favourite Color?</h1>
+<label>
+  <input type="checkbox" name="color" value="red" bind:group={favColor} />
+  Red
+</label>
+<label>
+  <input type="checkbox" name="color" value="green" bind:group={favColor} />
+  Green
+</label>
+<label>
+  <input type="checkbox" name="color" value="blue" bind:group={favColor} />
+  Blue
+</label>
