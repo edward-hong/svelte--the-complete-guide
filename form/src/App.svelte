6a5fe672b6a38961146309f1a1617c4f8b1b9806
edward-hong <edward.hong527@gmail.com>
2020-12-03 18:29:09 +1300

    S08L02 Two-Way Binding Refresher

---
+++ form/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  let val = ''
+
+  $: console.log(val)
+
+  function setValue(event) {
+    val = event.target.value
+  }
+</script>
+
+<style>
+
+</style>
+
+<!-- <input type="text" value={val} on:input={setValue} /> -->
+<input type="text" bind:value={val} />
