2868f240261260c198e7d021a721fa239564ac45
edward-hong <edward.hong527@gmail.com>
2020-12-03 22:14:21 +1300

    S09L04 Adding Some Validation Logic

--- meetus/src/Meetups/EditMeetup.svelte
+++ meetus/src/Meetups/EditMeetup.svelte
@@ -3,8 +3,10 @@
   import TextInput from '../UI/TextInput.svelte'
   import Button from '../UI/Button.svelte'
   import Modal from '../UI/Modal.svelte'
+  import { isEmpty } from '../helpers/validation.js'
 
   let title = ''
+  let titleValid = false
   let subtitle = ''
   let description = ''
   let imageUrl = ''
@@ -12,6 +14,8 @@
   let email = ''
 
   const dispatch = createEventDispatcher()
+
+  $: titleValid = !isEmpty(title)
 
   function submitForm() {
     dispatch('save', {
@@ -40,7 +44,7 @@
     <TextInput
       id="title"
       label="Title"
-      valid={true}
+      valid={titleValid}
       validityMessage="Please enter a valid title."
       value={title}
       on:input={(e) => (title = e.target.value)} />
