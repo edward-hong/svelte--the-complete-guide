cd2ce727e6469bf5b6be3ec302d110956ddae912
Edward Hong <edward.hong527@gmail.com>
2020-12-10 20:07:59 +1300

    S17L09 Migrating the Meetup Project Files into the Sapper Project

---
+++ sapper/src/helpers/validation.js
@@ -0,0 +1,8 @@
+export function isEmpty(val) {
+  return val.trim().length === 0
+}
+
+export function isValidEmail(val) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(String(val).toLowerCase())
+}
---
+++ sapper/src/meetups-store.js
@@ -0,0 +1,41 @@
+import { writable } from 'svelte/store'
+
+const meetups = writable([])
+
+const customMeetupsStore = {
+  subscribe: meetups.subscribe,
+  setMeetups: (meetupArray) => {
+    meetups.set(meetupArray)
+  },
+  addMeetup: (meetupData) => {
+    meetups.update((items) => {
+      return [meetupData, ...items]
+    })
+  },
+  updateMeetup: (id, meetupData) => {
+    meetups.update((items) => {
+      const meetupIndex = items.findIndex((i) => i.id === id)
+      const updatedMeetup = { ...items[meetupIndex], ...meetupData }
+      const updatedMeetups = [...items]
+      updatedMeetups[meetupIndex] = updatedMeetup
+      return updatedMeetups
+    })
+  },
+  removeMeetup: (id) => {
+    meetups.update((items) => {
+      return items.filter((i) => i.id !== id)
+    })
+  },
+  toggleFavourite: (id) => {
+    meetups.update((items) => {
+      const updatedMeetup = { ...items.find((m) => m.id === id) }
+      updatedMeetup.isFavourite = !updatedMeetup.isFavourite
+      const meetupIndex = items.findIndex((m) => m.id === id)
+      const updatedMeetups = [...items]
+      updatedMeetups[meetupIndex] = updatedMeetup
+      return updatedMeetups
+    })
+  },
+}
+
+export default customMeetupsStore
