289752619e9b88929b9da9c2de9618dca7dddd73
Edward Hong <edward.hong527@gmail.com>
2020-12-10 13:38:08 +1300

    S16L05 Cross-Component Scripts

--- special-elements/src/FamilyNode.svelte
+++ special-elements/src/FamilyNode.svelte
@@ -1,17 +1,48 @@
-<script>
-  export let member
+<script context="module">
+  console.log('Runs once!')
+
+  let deactivateNode
 </script>
 
-<h1>{member.name}</h1>
+<script>
+  export let member
 
-{#if member.isParent}
-  <ul>
-    {#each member.children as child}
-      <li>
-        <svelte:self member={child} />
-      </li>
-    {/each}
-  </ul>
-{/if}
+  let isActive
+
+  console.log('Runs multiple times')
+
+  function deactivate() {
+    isActive = false
+  }
+
+  function activate() {
+    if (deactivateNode) {
+      deactivateNode()
+    }
+
+    isActive = true
+    deactivateNode = deactivate
+  }
+</script>
+
+<style>
+  .active {
+    color: red;
+  }
+</style>
+
+<div on:click={activate} class:active={isActive}>
+  <h1>{member.name}</h1>
+
+  {#if member.isParent}
+    <ul>
+      {#each member.children as child}
+        <li>
+          <svelte:self member={child} />
+        </li>
+      {/each}
+    </ul>
+  {/if}
+</div>
 
 <hr />
